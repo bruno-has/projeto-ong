e1f284d5f58e70e84140b1cfad9af35ddda5fa9e
brunohas <brunolbp18@gmail.com>
2025-10-24 21:09:29 -0300

    add arquivo JS

---
+++ js/script.js
@@ -0,0 +1,51 @@
+/* Menu responsivo */
+const btn = document.querySelector('.btn-nav')
+const lista = document.querySelector('.lista-nav')
+
+btn.addEventListener('click', () => {
+    lista.classList.toggle('mostrar')
+})
+
+/* Carrossel */
+
+const carrossel = document.querySelector('.carrossel')
+let index = 1
+
+function updateCarrossel() {
+    let cards = carrossel.children
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].style.zIndex = 1
+        cards[i].style.opacity = 0.3
+        cards[i].style.transform = 'translateX(100px) scale(0.8) rotateY(-30deg)'
+    }
+
+    const prev = (index - 1 + cards.length) % cards.length
+    const next = (index + 1) % cards.length
+
+    // Card anterior
+    cards[prev].style.transform = 'translateX(-100px) scale(0.8) rotateY(30deg)'
+    // Card central
+    cards[index].style.transform = 'translateX(0) scale(1) rotateY(0deg)'
+    cards[index].style.zIndex = 2
+    cards[index].style.opacity = 1
+    // Card seguinte
+    cards[next].style.transform = 'translateX(100px) scale(0.8) rotateY(-30deg)'
+}
+
+const btnVoltar = document.querySelector('.btn-voltar')
+const btnAvancar = document.querySelector('.btn-avancar')
+btnVoltar.addEventListener('click', () => {
+    index = (index - 1 + 3) % 3
+    updateCarrossel()
+})
+
+btnAvancar.addEventListener('click', () => {
+    index = (index + 1) % 3
+    updateCarrossel()
+})
+
+/* Simulação de botão */
+const btnRelatorio = document.querySelector('.btn-relatorio')
+btnRelatorio.addEventListener('click', () => {
+    alert("(Simulacão) Baixando Relatório de Transparência")
+})
